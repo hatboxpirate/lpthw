e18310524a8384a7d001f5efff5088beda462586
hatboxpirate <hatboxpirate@gmail.com>
2025-07-24 18:32:07 -0600

    initial commit

---
+++ ex5.py
@@ -0,0 +1,21 @@
+name = "James Kirk"
+age = 35
+height = 74 # inches
+weight = 180 # pounds
+eyes = "Brown"
+teeth = "White"
+hair = "Brown"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth}, depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+height_in_cm = height * 2.54
+weight_in_kg = weight * 0.454
+
+print(f"His height in cm is {height_in_cm} and his weight in kg is {weight_in_kg}.")
